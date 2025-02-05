048254b6415ddeeca2c337d26c814baaafb5975e
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2025-02-05 16:08:10 +0600

    Bean Life Cycle explained

--- demo-DI/src/main/java/kg/alatoo/demodi/DemoDiApplication.java
+++ demo-DI/src/main/java/kg/alatoo/demodi/DemoDiApplication.java
@@ -1,11 +1,13 @@
 package kg.alatoo.demodi;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class DemoDiApplication {
+public class DemoDiApplication implements BeanPostProcessor {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(DemoDiApplication.class, args);
@@ -14,4 +16,11 @@ public class DemoDiApplication {
         System.out.println(bean.sayHello());
     }
 
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof GreetingService) {
+            System.out.println(beanName);
+        }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
 }
--- demo-DI/src/main/java/kg/alatoo/demodi/MainController.java
+++ demo-DI/src/main/java/kg/alatoo/demodi/MainController.java
@@ -1,13 +1,27 @@
 package kg.alatoo.demodi;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class MainController {
+@Controller("myController")
+public class MainController implements
+        BeanPostProcessor
+//        BeanNameAware,
+//        ApplicationContextAware, InitializingBean, CommandLineRunner
+{
 
     private GreetingService greetingService;
+    private String beanName;
+    private ApplicationContext context;
 
 
     public MainController(
@@ -16,8 +30,47 @@ public class MainController {
     }
 
     public String sayHello() {
-        System.out.println("Hi from Controller");
+        System.out.println("Hi from Controller: " + beanName);
+//        System.out.println(String.join(", ", context.getBeanDefinitionNames()));
         return greetingService.getGreeting();
     }
 
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+//        if (bean instanceof GreetingService)
+//            System.out.printf("The bean %s in creating progress%n", beanName);
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    /*@Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.printf("The bean %s has been created%n", beanName);
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }*/
+
+
+
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+
+//    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("MainController properties set successfully");
+    }
+
+//    @PostConstruct
+    public void runOnInit() {
+        System.out.println("MainController initialized");
+    }
+
+//    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("CommandLineRunner in MainController");
+    }
 }
